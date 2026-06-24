22b406b67121d79ef7d1e06602a3b2110ec7878d
Moong <tmdals9154@gmail.com>
2026-06-24 21:22:47 +0900

    chore(wedding): OG 설명 문구를 수정한다

--- apps/wedding/src/app/layout.tsx
+++ apps/wedding/src/app/layout.tsx
@@ -57,7 +57,7 @@ const OG_TITLE = `${WEDDING.groom.name} ♥ ${WEDDING.bride.name}`;
 
 const OG_IMAGE_PATH = '/photos/02-og-source.png';
 
-const OG_DESCRIPTION = '축하해 주러 오시면 정말 큰 기쁨일 거예요. 오래도록 행복하게 잘 살겠습니다.';
+const OG_DESCRIPTION = '🌸 소중한 마음으로 축복해 주시면 감사하겠습니다. 🌸';
 // const OG_DESCRIPTION = '오셔서 축하해 주시면 더없이 기쁠 거예요. 오래도록 사이좋게 잘 살겠습니다.';
 // const OG_DESCRIPTION = '함께 축하해 주시면 정말 기쁠 거예요. 오래도록 웃으며 잘 살겠습니다.';
 
